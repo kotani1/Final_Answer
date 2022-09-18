553ee2ff175b3e0ef588932c2ee48edd0a652d3c
kotani <21011103@c.anabuki-college.net>
2022-09-19 05:41:08 +0900

    追加

--- Exercise_for_Pool/python/test.py
+++ Exercise_for_Pool/python/test.py
@@ -16,7 +16,9 @@ pattern = '''(...??[都道府県])((?:旭川|伊達|石狩|盛岡|奥州|田村|
 富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村|宮古|富良野|別府|佐伯|黒部|小諸|塩尻|玉野|
 周南)市|(?:余市|高市|[^市]{2,3}?)郡(?:玉村|大町|.{1,5}?)[町村]|(?:.{1,4}市)?[^町]{1,4}?区|.{1,7}?[市町村])(.+)'''
 
-num =21
+num =0
+page =10
+
 names =[]
 phone_numbers =[]
 emails =[]
@@ -27,69 +29,73 @@ localities =[]
 urls =[]
 ssls =[]
 
-while num <26:
-  url = 'https://r.gnavi.co.jp/area/jp/continental/rs/?p='+str(num)
+while num <50:
+  url = 'https://r.gnavi.co.jp/area/jp/continental/rs/?p='+str(page)
   response = requests.get(url)
   bs = BeautifulSoup(response.text, 'html.parser')
   a_tags = bs.find_all('a', class_ = 'style_titleLink__oiHVJ')
   for i in range(0,len(a_tags)):
+    if num==50:
+      break
     r = requests.get(a_tags[i].get("href"))
     b = BeautifulSoup(r.content, 'html.parser')
-    # table = b.find('table', class_ = 'basic-table')
-    mail = b.find(id = 'info-email')
-    if mail:
-      print(a_tags[i].get("href"))
-      num+=100
-      break
+    table = b.find('table', class_ = 'basic-table')
+    name = check_none(table.find(id="info-name"))
+    phone_number = check_none(table.find(class_="number"))
+    email = (table.find(string='お店に直接メールする'))
+    if email  == None:
+      email =''
+    else:
+      print(email)
+      email = email.parent.get("href")[7:]
+
+    region = check_none(table.find(class_="region"))
+    locality = check_none(table.find(class_="locality"))
+
+    result = re.match(pattern, region)
+    if result: #正規表現パターンにマッチした場合
+      prefecture = result.group(1) #都道府県
+      municipality = result.group(2)#市区町村
+      street_number = result.group(3) #その他
+
+    url = ''
+    ssl = ''
+    names += [name]
+    phone_numbers += [phone_number]
+    emails += [email]
+    prefectures += [prefecture]
+    municipalities += [municipality]
+    street_numbers += [street_number]
+    localities += [locality]
+    urls += [url]
+    ssls += [ssl]
+    print(num)
+    # gurunabi_list.append([name,phone_number,prefecture,municipalities,
+    # street_number,locality])
+    num+=1
     time.sleep(5)
-  print(num)
-  num+=1
-    # name = check_none(table.find(id="info-name"))
-    # phone_number = check_none(table.find(class_="number"))
-    # email = ''
-    # region = check_none(table.find(class_="region"))
-
-    # result = re.match(pattern, region)
-    # if result: #正規表現パターンにマッチした場合
-    #   prefecture = result.group(1) #都道府県
-    #   municipality = result.group(2)#市区町村
-    #   street_number = result.group(3) #その他
-
-    # locality = check_none(table.find(class_="locality"))
-    # url = ''
-    # ssl = ''
-    # names += [name]
-    # phone_numbers += [phone_number]
-    # emails += [email]
-    # prefectures += [prefecture]
-    # municipalities += [municipality]
-    # street_numbers += [street_number]
-    # localities += [locality]
-    # urls += [url]
-    # ssls += [ssl]
-    # # gurunabi_list.append([name,phone_number,prefecture,municipalities,
-    # # street_number,locality])
-
-
+  page+=1
     # if i==5:
     #   break
 
 
 
-# data = {
-#     '店舗名': names,
-#     '電話番号': phone_numbers,
-#     'メールアドレス': emails,
-#     '都道府県':prefectures,
-#     '市区町村':municipalities,
-#     '番地':street_numbers,
-#     '建物名':localities,
-#     'URL':urls,
-#     'SSL':ssls,
+data = {
+    '店舗名': names,
+    '電話番号': phone_numbers,
+    'メールアドレス': emails,
+    '都道府県':prefectures,
+    '市区町村':municipalities,
+    '番地':street_numbers,
+    '建物名':localities,
+    'URL':urls,
+    'SSL':ssls,
 
-# }
-# df = pd.DataFrame(data)
-# df.to_csv("C:/Users/81802/Exercise_for_Pool/python/sample.csv", index=False)
+}
+df = pd.DataFrame(data)
+df.to_csv("C:/Users/81802/FINAL_ANSWER/Exercise_for_Pool/python/sample.csv",index=False)
+
+print('終了')
 
 
 
